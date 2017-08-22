a70841a4fd96205771eeff32731fa3d149d78eaf
Bolei Zhang <bz824@nyu.edu>
2017-08-22 11:07:53 -0400

    session_api_util complete

--- frontend/map_my_dream.jsx
+++ frontend/map_my_dream.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {signup, login, logout} from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to My Website</h1>, root);
 });
