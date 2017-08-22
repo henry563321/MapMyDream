994bcfd7f152e952eed6a16ae285aa4c93897986
Bolei Zhang <bz824@nyu.edu>
2017-08-22 10:01:17 -0400

    Finish user model, create user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,37 @@
 class User < ApplicationRecord
+  validates :username, :email, :session_token, persence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credential(username, password)
+    user = User.find_by(username: username)
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
